8ad23fba0157fd964c92daed0bb8af0f40d23bf7
DHan <119727539+DHan315@users.noreply.github.com>
2026-09-24 00:52:48 -0700

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from .api_image_gen import APIImageGen
+
+NODE_CLASS_MAPPINGS = {
+    "APIImageGen": APIImageGen,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "APIImageGen": "API-Image-gen",
+}
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
